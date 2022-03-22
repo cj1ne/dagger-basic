e48fb5e3a71956291004d1b7763a4d83345df1ba
glen.jw <glen.jw@kakaocorp.com>
2022-03-23 00:18:31 +0900

    DaggerApplication 상속받도록 수정

--- app/src/main/java/com/glen/daggerbasic/presentation/MyApplication.kt
+++ app/src/main/java/com/glen/daggerbasic/presentation/MyApplication.kt
@@ -1,28 +1,17 @@
 package com.glen.daggerbasic.presentation
 
-import android.app.Application
 import com.glen.daggerbasic.di.AppComponent
 import com.glen.daggerbasic.di.DaggerAppComponent
 import dagger.android.AndroidInjector
-import dagger.android.DispatchingAndroidInjector
-import dagger.android.HasAndroidInjector
-import javax.inject.Inject
+import dagger.android.DaggerApplication
 
-class MyApplication : Application(), HasAndroidInjector {
-
-    @Inject
-    lateinit var androidInjector: DispatchingAndroidInjector<Any>
+class MyApplication : DaggerApplication() {
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.factory().create(applicationContext)
     }
 
-    override fun onCreate() {
-        super.onCreate()
-        DaggerAppComponent.factory().create(applicationContext).inject(this)
-    }
-
-    override fun androidInjector(): AndroidInjector<Any> {
-        return androidInjector
+    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+        return DaggerAppComponent.factory().create(applicationContext)
     }
 }
